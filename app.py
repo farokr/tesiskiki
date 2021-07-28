34cc135296f5608728b567f5bc40cebe4dc768bf
Faro KR <faro.rasyid@gmail.com>
2021-07-28 19:50:43 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -10,9 +10,21 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-from kmodes.kmodes import KModes
+#from kmodes.kmodes import KModes
+
+#import pickle
+import base64
 
 column = ['aging', 'subscribe', 'order', 'Tagihan']
+
+
+def get_table_download_link(df):
+    csv = df.to_csv(index=False,sep=';')
+    b64 = base64.b64encode(csv.encode()).decode()
+    new_filename = "datahasil.csv"
+    href = f'<a href="data:file/csv;base64,{b64}" download="{new_filename}">Download file hasil clustering</a>'
+    return href
+#end of proses_data
     
     
 def eda():
@@ -33,14 +45,17 @@ def eda():
 
 def kmeans():
     st.header('K-Means')
-    df_master = pd.read_csv('data_pelanggan.csv',sep=';')     
-    df1 = pd.read_csv('data_ready.csv',sep=',')
+    cols = ['Tagihan','aging','subscribe','order']
     
-    scaler = preprocessing.Normalizer()
-    model = scaler.fit(df1[['Tagihan','aging','subscribe','order']])
-    df1 = model.transform(df1[['Tagihan','aging','subscribe','order']])
-   
-     
+    df_master = pd.read_csv('data_pelanggan.csv',sep=';')     
+    df1 = df_master[cols].copy()
+    
+    scaler = preprocessing.StandardScaler()
+    model = scaler.fit(df1)
+    df1 = model.transform(df1)
+    pca = PCA(2) #mengubah menajdi 2 kolom
+    df1 = pca.fit_transform(df1) #Transform data
+    
     st.subheader('Pemilihan nilai K Menggunakan Elbow Method')
     distortions = []
     inertias = []
@@ -98,133 +113,111 @@ def kmeans():
     
 
     model = KMeans(n_clusters=k_value,random_state=101) # isnisialisasi Kmeans dgn  nilai K yg dipilih
-    label = model.fit_predict(df1) #proses Clustering
-    pca = PCA(2) #mengubah menajdi 2 kolom
-    dfnp = pca.fit_transform(df1) #Transform data
-    center = pca.fit_transform(model.cluster_centers_)
+    model.fit_predict(df1) #proses Clustering
+    label = model.predict(df1) #proses Clustering
+
+    #center = pca.fit_transform(model.cluster_centers_)
+    center = model.cluster_centers_
     
     #dibuat menjadi dataFrame
-    df_master['x1'] = dfnp[:,0]
-    df_master['y1'] = dfnp[:,1]
-    df_master['label'] = label
+    #df_master['x1'] = dfnp[:,0]
+    #df_master['y1'] = dfnp[:,1]
+    
+    df_master['x1'] = df1[:,0]
+    df_master['y1'] = df1[:,1]   
+    df_master['cluster'] = label
+    
     
     
     fig3= plt.figure()
-    sns.scatterplot(x='x1', y='y1',hue='label',data=df_master,alpha=1, s=40, palette='deep')
+    sns.scatterplot(x='x1', y='y1',hue='cluster',data=df_master,alpha=1, s=40, palette='deep')
     plt.scatter(x=center[:, 0], y=center[:, 1], s=100, c='black', ec='red',label='centroid')
     plt.legend(bbox_to_anchor=(1,1), loc="upper left")
     st.write(fig3)
     
     fig4= plt.figure()
-    sns.countplot(x ='label', data=df_master)
+    sns.countplot(x ='cluster', data=df_master)
     st.write(fig4)
     
-    cluster = df_master['label'].unique()
+    cluster = df_master['cluster'].unique()
     cluster.sort()
 
     
     choice = st.selectbox("Pilih Kluster",cluster)
-    res = df_master.loc[df_master['label'] == choice]
-    st.subheader('Kluster '+str(choice)+' '+str(res.shape))
+    res = df_master.loc[df_master['cluster'] == choice]
+    st.subheader('Cluster '+str(choice)+': '+str(res.shape[0])+' data')
     st.write(res.sample(10))
      
-     
-     
+    
+    
+    
+def apps():
+    cols = ['Tagihan','aging','subscribe','order']
+    k_value = int(st.text_input('Nilai K:',value=4))
+    data = st.file_uploader("Upload a Dataset", type=["csv"])
+    if data is not None:
+        
+        
+        df = pd.read_csv(data,sep=';')
+        st.dataframe(df)
+        
 
-def kmodes():
-    st.header('K-Modes')
-    df_master = pd.read_csv('data_pelanggan.csv',sep=';')
-    df = X = pd.read_csv('data_ready.csv',sep=',')
-    
-    scaler = preprocessing.Normalizer()
-    model = scaler.fit(df[['Tagihan','aging','subscribe','order']])
-    df = model.transform(df[['Tagihan','aging','subscribe','order']])
-    
-    cost = []
-    dbi = []
-    slh = []
-    K = range(2,9)
- 
-    for k in K:
-        kmode = KModes(n_clusters=k, init = "Cao", n_init = 1)
-        kmode.fit_predict(df)
-        cost.append(kmode.cost_)
-        #evaluasi silhouette
-        slh.append(metrics.silhouette_score(df,kmode.labels_))
-        #evaluasi DBI
-        dbi.append(metrics.davies_bouldin_score(df,kmode.labels_))
+        df1 = df[cols].copy() 
+        scaler = preprocessing.StandardScaler()
+        model = scaler.fit(df1)
+        df1 = model.transform(df1)
+        
+        pca = PCA(2)
+        df1 = pca.fit_transform(df1)
+
+        
+        model = KMeans(n_clusters=k_value,random_state=101)
+        model.fit(df1)
+        label = model.predict(df1)
+  
+        center = model.cluster_centers_
+        #center = pca.fit_transform(model.cluster_centers_)
+        
+        
+        #dibuat menjadi dataFrame
+        df['x1'] = df1[:,0]
+        df['y1'] = df1[:,1]
+        df['cluster'] = label
+        #st.write('Proses Dimulai...')
+       # for index, row in df.iterrows():
+            #st.write(str(row['NO'])+'... cluster: ',str(row['cluster']))
+        #st.write('Proses Selesai')
+        st.dataframe(df)
+        
+        fig3= plt.figure()
+        sns.scatterplot(x='x1', y='y1',hue='cluster',data=df,alpha=1, s=40, palette='deep')
+        plt.scatter(x=center[:, 0], y=center[:, 1], s=100, c='black', ec='red',label='centroid')
+        plt.legend(bbox_to_anchor=(1,1), loc="upper left")
+        st.write(fig3)
+
+        fig4= plt.figure()
+        sns.countplot(x ='cluster', data=df)
+        st.write(fig4)
+        
+        st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+        
         
     
-    st.text('The Elbow Method using K-modes Cost')
-    fig2a = plt.figure(figsize=(4,2))
-    plt.plot(K, cost, 'bx-')
-    plt.xlabel('Values of K')
-    plt.ylabel('Cost')
-    st.write(fig2a)
-    
-    st.text('Metode DBI')
-    fig2 = plt.figure(figsize=(4,2))
-    plt.plot(K, dbi, 'bx-')
-    plt.xlabel("Nilai K")
-    plt.ylabel('DBI')
-    st.write(fig2)
-    
-    st.text('Metode Silhouette')
-    fig2 = plt.figure(figsize=(4,2))
-    plt.plot(K, slh, 'bx-')
-    plt.xlabel("Nilai K")
-    plt.ylabel('Silhouette')
-    st.write(fig2)
-
-    st.header('K-Modes Modelling')
-    
-    k_value  = st.slider('Nilai K', min_value=3, max_value=10, step=1, value=4)
-    model = KModes(n_clusters=k_value, init = "Cao", n_init = 1, verbose=1,random_state=101)# isnisialisasi Kmodes dgn  nilai K yg dipilih
-    label = model.fit_predict(X) #proses Clustering
-    
-    
-    pca = PCA(2) #mengubah menajdi 2 kolom
-    dfnp = pca.fit_transform(X) #Transform data
-    center = pca.fit_transform(model.cluster_centroids_)
-    
-    #dibuat menjadi dataFrame
-    df_master['x1'] = dfnp[:,0]
-    df_master['y1'] = dfnp[:,1]
-    df_master['label'] = label
-    
-    
-    fig3a= plt.figure()
-    sns.scatterplot(x='x1', y='y1',hue='label',data=df_master,alpha=1, s=40, palette='deep')
-    plt.scatter(x=center[:, 0], y=center[:, 1], s=100, c='black', ec='red',label='centroid')
-    plt.legend(bbox_to_anchor=(1,1), loc="upper left")
-    st.write(fig3a)
-    
-    fig4a= plt.figure()
-    sns.countplot(x ='label', data=df_master)
-    st.write(fig4a)
-
-    cluster2 = df_master['label'].unique()
-    cluster2.sort()
-    
-    choice2 = st.selectbox("Pilih Kluster",cluster2)
-    res = df_master.loc[df_master['label'] == choice2]
-    st.subheader('Kluster '+str(choice2)+' '+str(res.shape))
-    st.write(res.sample(10))
-    
-    
+#end of apps
     
 def main():
+    """ Streamlit Pelanggaran Pilkada Jabar """
 
-    activities = ['EDA','K-Means','K-Modes']	
+    activities = ['EDA','K-Means','Aplikasi Perhitungan']
     choice = st.sidebar.selectbox("Select Activities",activities)
     
     if choice == 'EDA':
         eda()
     elif choice == 'K-Means':
         kmeans()
-    elif choice == 'K-Modes':
-        kmodes()
+    elif choice == 'Aplikasi Perhitungan':
+        apps()
         
 
 if __name__ == '__main__':
-	main()
+    main()
